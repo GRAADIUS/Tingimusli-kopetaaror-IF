afe4b608bf92a0b907fde9680f2daadb6ffba1e5
opilane <opilane@HP-CZC6297B9P>
2022-12-19 14:10:41 +0200

    Add project files.

---
+++ Tingimuslik_opetaaror_IF.py
@@ -0,0 +1,102 @@
+from math import *
+from random import *
+##1
+#try:
+#    print("tere!")
+#    nimi=str(input("kudas sinu nimi? "))
+#    if nimi.upper()=="JUKU":
+#        print("lähme kinno!")
+#        vanus=int(input("aitäh, aga kui vana sa oled? "))
+#        if vanus<0 or vanus>100:
+#           print("viga andmetega")
+#        elif vanus<6:
+#            print("ok, peame ostma tasuta")
+#        elif vanus>=6 and vanus<14:
+#            ptint("ok, peame ostma lastepilet")
+#        elif vanus>=14 and vanus<65:
+#            print("ok, peame ostma täispilet")
+#        elif vanus>=65:
+#            print("ok, peame ostma sooduspilet")
+#    else:
+#        print("otsime Juku")
+#except: 
+#    print("duck")
+##2
+#try:
+#    n1=input("sisestage esimese isiku nimi ")
+#    n2=input("sisestage teise isiku nimi ")
+#    if n1.isalpha()==true and n2.isalpha()==true:
+#        if n1.lower=="juku" and n2.lower=="duck":
+#            print(f"{n1} ja {n2} lähedale viskama")
+#        else:
+#            print("duck")
+#    else:
+#        print("duck")
+#except: 
+#    print("duck")
+##3
+#try:
+#    a=input("nimetage põranda pikkus ")
+#    b=input("nimetage põranda laius ")
+#    s=a*b
+#    küsimus=input("kas soovite remonti teha? ")
+#    if küsimus=="jah":
+#        money=input("kui palju ruut on? ")
+#        mon=s*mponey
+#        print(f"see läheb maksma {mon}$")
+#    else:
+#        print("kahju")
+#except: 
+#    print("duck")
+##4
+#try:
+#    mik=input("sisestage raha")
+#    if mik>700:
+#        proc=mik-mik*0.3
+#        print(f"{mik} -30%")
+#    else:
+#        print("rahaga on kitsas")
+#except: 
+#    print("duck")
+##5
+#try:
+#    temp=input("kui palju kraade?")
+#    if int(temp)<18:
+#        print("külm")
+#    else:
+#        print("soojalt")
+#except: 
+#    print("duck")
+##6
+#try:
+#    pikk=float(input("mis on su pikkus? "))
+#    if pikk<161:
+#        print("teie pikkus on alla keskmise")
+#    elif pikk>=161 and pikk<191:
+#        print("oled keskmist kasvu")
+#    else:
+#        print("teie pikkus on üle keskmise")
+#except:
+#    print("duck")
+##7
+#try:
+#    mean=input("sa on meessoost? ")
+#    if mean=="jah":
+#        pikkus=float(input("mis on su pikkus? "))
+#        if pikkus<161:
+#            print("teie pikkus on alla keskmise")
+#        elif pikkus>=161 and pikkus<191:
+#            print("oled keskmist kasvu")
+#        else:
+#            print("teie pikkus on üle keskmise")
+#    else:
+#        pikkus=input("mis on su pikkus? ")
+#        if pikkus<149:
+#            print("teie pikkus on alla keskmise")
+#        elif pikkus>=149 and pikkus<179:
+#            print("oled keskmist kasvu")
+#        else:
+#            print("teie pikkus on üle keskmise")
+#except:
+#    print("duck")
+##8
